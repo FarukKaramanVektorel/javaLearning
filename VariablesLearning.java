7fd5bbc5c7c085a26352e2a6bc4c9e2667662a38
Faruk Karaman <101044778+Faruk-Karaman@users.noreply.github.com>
2023-05-27 16:58:58 +0300

    Variables

---
+++ VariablesLearning.java
@@ -0,0 +1,29 @@
+//https://www.farukkaraman.com/java-temel-veri-tipleri/
+public class VariablesLearning {public static void main(String[] args) {
+    // Tam sayılar
+    byte byteDegisken = 10;
+    short shortDegisken = 100;
+    int intDegisken = 1000;
+    long longDegisken = 1000000L;
+
+// Ondalık sayılar
+    float floatDegisken = 3.14f;
+    double doubleDegisken = 2.71828;
+
+// Mantıksal değerler
+    boolean booleanDegisken = true;
+
+// Karakter
+    char charDegisken = 'A';
+    System.out.println(byteDegisken);
+    System.out.println(shortDegisken);
+    System.out.println(intDegisken);
+    System.out.println(longDegisken);
+    System.out.println(floatDegisken);
+    System.out.println(doubleDegisken);
+    System.out.println(booleanDegisken);
+    System.out.println(charDegisken);
+
+
+}
+}
